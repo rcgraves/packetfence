32ebb435db44453032a3768a6528767dd6615438
Olivier Bilodeau <olivier@bottomlesspit.org>
2012-05-17 15:59:00 -0400

    refactoring (exception, practices) + create works
    * no JSON in entry of data, only POST key=value
    * added _ref to references
    * newer shinier try {} catch {}; syntax

--- html/configurator/lib/configurator.pm
+++ html/configurator/lib/configurator.pm
@@ -51,7 +51,7 @@ __PACKAGE__->config(
     'View::JSON' => {
        allow_callback  => 1,    # defaults to 0
        callback_param  => 'cb', # defaults to 'callback'
-       expose_stash    => [ qw(result error interfaces switches) ], # defaults to everything
+       expose_stash    => [ qw(status_msg error interfaces switches) ], # defaults to everything
     },
 );
 
