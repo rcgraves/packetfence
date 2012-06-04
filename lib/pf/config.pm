5653b05b595227482806708cf6e59886c2b1b7f7
Olivier Bilodeau <olivier@bottomlesspit.org>
2012-06-04 09:40:29 -0400

    config reload support and a user
    - new load_config sub in pf::config
    - Model adapter exposes it as reloadConfiguration
    - wizard step 5 calls it

--- lib/pf/config.pm
+++ lib/pf/config.pm
@@ -11,6 +11,9 @@ pf::config - PacketFence configuration
 pf::config contains the code necessary to read and manipulate the 
 PacketFence configuration files.
 
+It automatically imports gazillions of globals into your namespace. You 
+have been warned.
+
 =head1 CONFIGURATION AND ENVIRONMENT
 
 Read the following configuration files: F<log.conf>, F<pf.conf>, 
@@ -84,6 +87,7 @@ BEGIN {
         is_vlan_enforcement_enabled is_inline_enforcement_enabled
         is_in_list
         $LOG4PERL_RELOAD_TIMER
+        load_config
     );
 }
 
@@ -236,30 +240,44 @@ our $TIME_MODIFIER_RE = qr/[smhDWMY]/;
 our $BANDWIDTH_DIRECTION_RE = qr/IN|OUT|TOT/;
 our $BANDWIDTH_UNITS_RE = qr/B|KB|MB|GB|TB/;
 
-try {
+# constants are done, let's load the configuration
+load_config();
 
-    readPfConfigFiles();
-
-    # Captive Portal constants
-    Readonly %CAPTIVE_PORTAL => (
-        "NET_DETECT_INITIAL_DELAY" => floor($Config{'trapping'}{'redirtimer'} / 4),
-        "NET_DETECT_RETRY_DELAY" => 2,
-        "NET_DETECT_PENDING_INITIAL_DELAY" => 2 * 60,
-        "NET_DETECT_PENDING_RETRY_DELAY" => 30,
-        "TEMPLATE_DIR" => "$install_dir/html/captive-portal/templates",
-    );
-
-    readNetworkConfigFile();
-
-    readFloatingNetworkDeviceFile();
-
-} catch {
-
-    $logger->logdie("Fatal error preventing configuration to load. Please review your configuration. Error: $_");
-};
-
+=head1 SUBROUTINES
 
 =over
+
+=item load_config
+
+Load configuration. Can be used to reload it too.
+
+WARNING: This has been recently introduced and was not tested with our 
+multi-threaded daemons.
+
+=cut
+sub load_config {
+
+    try {
+
+        readPfConfigFiles();
+
+        # Captive Portal constants
+        Readonly %CAPTIVE_PORTAL => (
+            "NET_DETECT_INITIAL_DELAY" => floor($Config{'trapping'}{'redirtimer'} / 4),
+            "NET_DETECT_RETRY_DELAY" => 2,
+            "NET_DETECT_PENDING_INITIAL_DELAY" => 2 * 60,
+            "NET_DETECT_PENDING_RETRY_DELAY" => 30,
+            "TEMPLATE_DIR" => "$install_dir/html/captive-portal/templates",
+        );
+
+        readNetworkConfigFile();
+        readFloatingNetworkDeviceFile();
+
+    } catch {
+
+        $logger->logdie("Fatal error preventing configuration to load. Please review your configuration. Error: $_");
+    };
+}
 
 =item readPfConfigFiles -  pf.conf.defaults & pf.conf
 
